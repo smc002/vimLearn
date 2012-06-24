20b076bf7ed0c2cdbc5196309e018ae98cd40206
sunmengchao <sunmengchao@gmail.com>
2012-06-24 11:01:48 +0800

    try

---
+++ test.c
@@ -0,0 +1,95 @@
+#include <>
+
+int main()
+{
+}
+
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func zplword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func kehword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
+func keyword blablabla
+{
+
+sjflajflajfld;saf;
+
+alfjd;af;a
+;;
+}
